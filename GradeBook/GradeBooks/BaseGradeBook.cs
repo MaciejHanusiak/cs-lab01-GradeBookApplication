90cba86b25c9eceb93623879379932f11c9fbaf7
Maciej Hanusiak <maciej.hanusiak@microsoft.wsei.edu.pl>
2023-04-04 15:41:04 +0200

    2nd test passed

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,8 +9,10 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
+    
     public class BaseGradeBook
     {
+        public GradeBookType Type { get; set; }         //I'm not sure about that
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
